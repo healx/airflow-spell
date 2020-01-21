14ca419c4f2372ce051903578cf2ead4c5e4d3d6
Dan O'Donovan <dan.odonovan@gmail.com>
2020-01-21 13:26:48 +0000

    Testing DAG

---
+++ integration-test/dags/spell_submit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow_spell import HelloOperator
+
+
+default_args = {
+    'depends_on_past': False,
+    'start_date': datetime(2019, 10, 13),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=10)
+}
+
+
+with DAG("Airflow-Spell-Testing", default_args=default_args):
+    hello_task = HelloOperator(task_id='sample-task', name='foo_bar')
