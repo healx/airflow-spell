7e3e7312866178cd1d94bd37f80393c6fcc9a957
Dan O'Donovan <dan.odonovan@gmail.com>
2022-03-17 12:08:46 +0000

    Add check_run_complete tests

--- tests/hooks/test_spell_client.py
+++ tests/hooks/test_spell_client.py
@@ -1,5 +1,13 @@
+import inspect
 from timeit import default_timer
-from precisely import assert_that, has_attr, less_than
+from typing import Callable
+from unittest.mock import MagicMock
+
+from airflow.exceptions import AirflowException
+from precisely import assert_that, has_attr, is_instance, less_than, raises
+import pytest
+from spell.client.runs import RunsService
+
 from airflow_spell import SpellClient
 from airflow_spell.hooks.spell_client import _delay
 
@@ -16,3 +24,40 @@ def test_delay_delays_for_duration_within_bounds():
     average_duration = (default_timer() - start) / 10
 
     assert_that(average_duration, less_than(1))
+
+
+@pytest.fixture
+def spell_client() -> SpellClient:
+    client = SpellClient()
+    client.MAX_RETRIES = 2
+    return client
+
+
+def mock_get_run(status) -> Callable:
+    def mock_func(_, __):
+        return MagicMock(status=status)
+
+    return mock_func
+
+
+POSSIBLE_INCOMPLETE_SPELL_STATUS = [
+    member[1]
+    for member in inspect.getmembers(RunsService, lambda value: isinstance(value, str))
+    if "." not in member[1] and member[1] is not "complete"
+]
+
+
+class TestRunComplete:
+    def test_run_complete_does_not_raise_exception(self, monkeypatch, spell_client):
+        monkeypatch.setattr(
+            SpellClient, "_get_run", mock_get_run(status=RunsService.COMPLETE)
+        )
+        spell_client.check_run_complete(run_id="test1")
+
+    @pytest.mark.parametrize("status", POSSIBLE_INCOMPLETE_SPELL_STATUS)
+    def test_run_not_complete_raises_exception(self, status, monkeypatch, spell_client):
+        monkeypatch.setattr(SpellClient, "_get_run", mock_get_run(status=status))
+        assert_that(
+            lambda: spell_client.check_run_complete(run_id="test1"),
+            raises(is_instance(AirflowException)),
+        )
